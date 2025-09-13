f3a68bd85d931d20c79e46d8a3a865cf54c15d7f
jnadeau007 <james4redsox@gmail.com>
2025-09-13 11:27:15 -0400

    Added movement capability for the player object

--- player.py
+++ player.py
@@ -23,3 +23,25 @@ class Player(CircleShape):
         # use the list points (self.a , self.b, self.c ) to draw the triangle
         # line width of 2
         pygame.draw.polygon(screen, 'white', self.triangle(), width=2)
+
+    def rotate(self, amount):
+        self.rotation += amount
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-PLAYER_TURN_SPEED * dt)
+        if keys[pygame.K_d]:
+            self.rotate(PLAYER_TURN_SPEED * dt)
+        
+        if keys[pygame.K_w]:
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+   
